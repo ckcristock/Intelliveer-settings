36d1f3e93d40eab04e319cad2ba1fd901ce03f53
Cristian <cristian@tradebuilderinc.com>
2022-11-20 05:42:02 -0500

    commit 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { NavbarComponent } from './modules/navbar/navbar.component';
+import { LeftMenuComponent } from './modules/left-menu/left-menu.component';
+import { RightMenuComponent } from './modules/right-menu/right-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    NavbarComponent
+    NavbarComponent,
+    LeftMenuComponent,
+    RightMenuComponent
   ],
   imports: [
     BrowserModule,
