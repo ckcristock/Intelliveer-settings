26df94e9b90c3d05292f4037bada98ecefcd2423
Cristian <ckcristock@gmail.com>
2023-08-07 04:04:42 -0500

    15 - onboarding service

--- src/app/services/onboarding.service.ts
+++ src/app/services/onboarding.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OnboardingService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getBGnLE() {
+    return this.http.get("https://rickandmortyapi.com/api/character");
+  }
+
+  getLCnPR() {
+    return this.http.get("https://rickandmortyapi.com/api/location");
+  }
 }
